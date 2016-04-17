bffdad85e0e2308eb0762bef9305a2b7e6ea8474
Chang Ming-Han <minghan@ming-hantekimacbook-pro.local>
2016-04-17 13:09:11 +0800

    Initial Commit

---
+++ Fraction_Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fraction_Calculator
+//
+//  Created by Ming-Han Chang on 2016/4/17.
+//  Copyright © 2016年 Ming-Han Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
